32464dfa4fc9ccc2f12bc43d0636d2301970f940
jasper <240023810@qq.com>
2019-01-23 14:09:35 +0800

    本地环境记录sql

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(app()->isLocale()){
+        if(app()->environment()=='local'){
             DB::listen(function ($query) {
                 $sql = $query->sql;
                 $bindings = $query->bindings;
