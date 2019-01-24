7ecd2f8ef7b1d6b591335e1a1d1c4b93e01f46e8
jasper <240023810@qq.com>
2019-01-24 09:50:38 +0800

    修改关注时的信息

--- app/Http/Controllers/WeChatController.php
+++ app/Http/Controllers/WeChatController.php
@@ -10,7 +10,7 @@ class WeChatController extends Controller
     {
         $app = app('wechat.official_account');
         $app->server->push(function($message){
-            return "欢迎关注 overtrue！";
+            return "欢迎关注！";
         });
 
         return $app->server->serve();
