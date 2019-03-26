9cb424a44690ef9fa349a2d10d09f35e946ebda2
liuqiankun <liuqiankun@kooap.com>
2019-03-26 18:10:18 +0800

    添加根据id获取详情接口

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 //游戏攻略过审首页
 Route::get('/gamestrategy','GameStrategyController@showlists');
 Route::get('/gamestrategy/{appletId}','GameStrategyController@getItemByAppletId');
+Route::get('/gamestrategy/detail/{id}','GameStrategyController@getItemDetailById');
 
 //游戏攻略首页
 Route::get('/gamehome','HomeController@getHomeDatas');
