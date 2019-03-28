4375cdc37bb9ccc74e75b8747109656db0f4357c
liuqiankun <liuqiankun@kooap.com>
2019-03-28 15:51:51 +0800

    攻略页面添加appid搜索功能

--- app/Admin/Controllers/GameStrategyController.php
+++ app/Admin/Controllers/GameStrategyController.php
@@ -107,6 +107,12 @@ class GameStrategyController extends Controller
         });
 //        $grid->created_at('Created at');
 //        $grid->updated_at('Updated at');
+        $grid->filter(function($filter){
+            // 去掉默认的id过滤器
+            $filter->disableIdFilter();
+            // 在这里添加字段过滤器
+            $filter->like('appletId', 'appletId');
+        });
         $grid->model()->orderBy('id','desc');
         return $grid;
     }
