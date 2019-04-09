4cdec98681885b1e21c1707e51fe7606803ff88d
liuqiankun <liuqiankun@kooap.com>
2019-04-09 16:23:49 +0800

    多余筛选条件

--- app/Admin/Controllers/VisitPageController.php
+++ app/Admin/Controllers/VisitPageController.php
@@ -103,11 +103,6 @@ class VisitPageController extends Controller
             $filter->equal('ref_date', '日期')->datetime(['format' => 'YYYYMMDD']);
         });
         $grid->model()->orderBy('ref_date', 'desc');
-
-        $grid->filter(function($filter){
-            $filter->disableIdFilter();
-            $filter->equal('ref_date', '日期')->datetime(['format' => 'YYYYMMDD']);
-        });
         return $grid;
     }
 
