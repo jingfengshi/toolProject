34cdcb3e3780583728b53e0a54b4da2d0b5a6954
liuqiankun <liuqiankun@kooap.com>
2019-04-12 09:36:22 +0800

    添加id排序，消除重复

--- app/Admin/Controllers/DailyVisitTrendController.php
+++ app/Admin/Controllers/DailyVisitTrendController.php
@@ -107,8 +107,8 @@ class DailyVisitTrendController extends Controller
             $filter->disableIdFilter();
             $filter->equal('ref_date', '日期')->datetime(['format' => 'YYYYMMDD']);
         });
-        $grid->model()->orderBy('ref_date', 'desc');
         $grid->model()->orderBy('visit_uv_new', 'desc');
+        $grid->model()->orderBy('id', 'desc');
         return $grid;
     }
 
