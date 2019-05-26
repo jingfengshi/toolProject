12e047923ef8a9404b28ce87d9be80bcb3b49e5b
jasper <240023810@qq.com>
2019-05-26 17:10:39 +0800

    获取用户openid

--- app/Http/Middleware/getUserOpenId.php
+++ app/Http/Middleware/getUserOpenId.php
@@ -24,7 +24,8 @@ class getUserOpenId
         $appSecret = config('admin.wechat_ff.secret');
         $openId =  new getOpenID($appId,$appSecret);
         $url = config('admin.wechat_ff.host');
-        dd($request->spread);
+        dump($request->fullUrlWithQuery());
+        dd($request->url());
         $openId->get_authorize_url("snsapi_base", $url);
 
 
