3fa7dcf343244a8829a9f27797905604443182f6
jasper <240023810@qq.com>
2019-05-27 16:36:58 +0800

    测试

--- app/Http/Controllers/LandController.php
+++ app/Http/Controllers/LandController.php
@@ -8,6 +8,9 @@ class LandController extends Controller
 {
     public function index($number,$spread)
     {
+
+        $open_id = request('openid');
+        dd($open_id);
         $origin_url = base64_decode($spread);
 
         return view('land',compact('origin_url'));
