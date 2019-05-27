ffe93137f7c794c58012a396b600accc3645eeac
jasper <240023810@qq.com>
2019-05-27 16:34:25 +0800

    测试

--- app/Http/Controllers/SpreadController.php
+++ app/Http/Controllers/SpreadController.php
@@ -10,9 +10,9 @@ class SpreadController extends Controller
     public function index($spread)
     {
         if(LandDomain::where('is_dead',false)->exists()){
-            dd(request('open_id'));
+            dd(request('openid'));
             $landUrl ='http://'.LandDomain::where('is_dead',false)->first()->value('url');
-            $final_url =$landUrl.'/rtyythggfghssdfxzvcdfghdhgfdhewqsdf/'.rand(111111111,999999999).'/'.$spread.'?open_id=';
+            $final_url =$landUrl.'/rtyythggfghssdfxzvcdfghdhgfdhewqsdf/'.rand(111111111,999999999).'/'.$spread.'?openid=';
             header('Location: '.$final_url);//
         }else{
             header('Location: http://baidu.com');//如果没有落地域名则跳转到百度
